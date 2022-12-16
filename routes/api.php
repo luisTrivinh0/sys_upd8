cf9f79339a7e4771c251276b083d4d9f2b6ae661
luisTrivinh0 <luis.trivinho@icloud.com>
2022-12-16 09:53:42 -0300

    Funções API

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/clientes/create', 'App\Http\Controllers\ClientesController@store');
+Route::post('/clientes/update', 'App\Http\Controllers\ClientesController@update');
+Route::get('/clientes/delete', 'App\Http\Controllers\ClientesController@delete');
+
+
+//Rotas de Clientes
+Route::post('/pedidos/create', 'App\Http\Controllers\PedidosController@store');
+Route::post('/pedidos/update', 'App\Http\Controllers\PedidosController@update');
+Route::get('/pedidos/delete', 'App\Http\Controllers\PedidosController@delete');
